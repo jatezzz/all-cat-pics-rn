134d43011ad95f718a74456c6ea02c7ddace08cb
jatezzz <johnandrestrujillo@gmail.com>
2024-03-10 19:12:22 -0500

    Initial router added.

---
+++ src/app/cat/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export const Page = () => {
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+};
+
+export default Page;
--- src/screens/List/List.screen.tsx
+++ src/screens/List/List.screen.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { ListScreenProps as Props } from "./List.screen.types";
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
 
 const ListScreen: React.FC<Props> = () => {
   return (
     <>
       <Text>Hola mundo</Text>
+      <Link href={`/cat`} asChild>
+        <TouchableOpacity>
+          <View>
+            <Text>Test the cat</Text>
+          </View>
+        </TouchableOpacity>
+      </Link>
     </>
   );
 };
